77b291d4624a5b2d37b5995d8f8e0d3c621bc553
Jadyn Rutz <jaonhax@gmail.com>
2023-02-05 20:44:03 -0600

    Add comments

--- Building Blocks/code.js
+++ Building Blocks/code.js
@@ -2,6 +2,7 @@
 function runTimer(outputElem){
     var currTime = 50;
 
+    // Iterate through the times
     displayTime(currTime, outputElem);
     setTimeout(function(){currTime -= 5; displayTime(currTime, outputElem);}, 5000);
     setTimeout(function(){currTime -= 5; displayTime(currTime, outputElem);}, 10000);
@@ -17,7 +18,9 @@ function runTimer(outputElem){
 
 // Display function for mission control timer
 function displayTime(currTime, outputElem){
-    console.log(currTime)
+    // console.log(currTime)
+
+    // Determine whether to display time or blastoff
     if (currTime > 0) {
         outputElem.innerHTML = currTime.toString();
     } else {
@@ -25,28 +28,33 @@ function displayTime(currTime, outputElem){
     }
 }
 
-// Calculate pi using random rolls
-function dicePi(n, outputElem){
+// Calculate pi using random numbers
+function randomPi(n, outputElem){
     var pi = 0;
     var inSets = 0;
     var outSets = 0;
 
     // Do the actual rolling and calculations for pi
     function rollForPi(inSets, outSets, pi){
+        // Roll 2 dice
         var roll1 = Math.random();
         var roll2 = Math.random();
 
+        // Check whether rolls are points within a circle of radius 1 around origin
         if (roll1 ** 2 + roll2 ** 2 < 1) {
+            // If so, add 1 to the number of sets inside the area
             inSets++;
             pi = 4 * inSets / (inSets + outSets);
             return inSets, outSets, pi;
         }
 
+        // Otherwise add 1 to the number of sets outside the area
         outSets++;
         pi = 4 * inSets / (inSets + outSets);
         return inSets, outSets, pi;
     }
 
+    // Adjust official variables and display the result for pi
     for (let i = 0; i < n; i++) {
         setTimeout(function(){inSets, outSets, pi = rollForPi(inSets, outSets, pi); outputElem.innerHTML = pi.toString();}, i);
     }
